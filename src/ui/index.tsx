61f539c47f6b3fab4ffa2ce4c2684df025691127
Patrick Dugan <dugan.pat@gmail.com>
2025-04-30 10:32:47 -0700

    Base commit

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import UIApp from "./app";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app';
 
-document.addEventListener("DOMContentLoaded", () => {
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(<UIApp />);
+document.addEventListener('DOMContentLoaded', () => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(<App />);
 });
---
+++ src/ui/screens/plugin.tsx
@@ -0,0 +1,55 @@
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import Navigation from '../navigation';
+
+function PLuginScreen({}) {
+  const [option, setOption] = useState('');
+
+  const handleOptionChange = (event) => {
+    setOption(event.target.value);
+  };
+
+  useEffect(() => {
+    if (option) {
+      window.parent.postMessage(
+        {
+          pluginMessage: {
+            type: 'selected',
+            option: option,
+          },
+        },
+        '*',
+      );
+    }
+
+    window.onmessage = (event) => {
+      const msg = event.data.pluginMessage;
+      switch (msg.type) {
+        case 'optionStored':
+          console.log('optionStored');
+          console.log(msg);
+      }
+    };
+  }, [option]);
+
+  return (
+    <div className='px-4 py-1'>
+      <Navigation />
+      <h1 className='text-xl font-bold'>Page 1</h1>
+      <label className='block'>
+        <span className='text-gray-700'>Select</span>
+        <select
+          name='reference-language'
+          id='reference-language'
+          onChange={handleOptionChange}
+          className='form-select block w-full mt-1 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-xs'
+        >
+          <option value='option1'>Option 1</option>
+          <option value='option2'>Option 2</option>
+        </select>
+      </label>
+    </div>
+  );
+}
+
+export default PLuginScreen;

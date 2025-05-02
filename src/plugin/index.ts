352fc222fa9f98b16ffc6dff06cc49461dbd6397
Patrick Dugan <dugan.pat@gmail.com>
2025-05-02 09:13:51 -0700

    Some more config

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -2,17 +2,17 @@ figma.showUI(__html__);
 
 figma.ui.onmessage = async (msg) => {
   switch (msg.type) {
-    case "selected":
-      console.log("option selected.");
+    case 'selected':
+      console.log('option selected.');
       console.log(msg);
 
       await figma.clientStorage
-        .setAsync("option", msg.option)
+        .setAsync('option', msg.option)
         .catch(() => {
-          console.log("An error occurred while storing the option.");
+          console.log('An error occurred while storing the option.');
         })
         .finally(() => {
-          figma.ui.postMessage({ type: "optionStored" });
+          figma.ui.postMessage({ type: 'optionStored' });
         });
   }
 };

b0468afbaf69ab16b7f7cff783a96555a0303617
Patrick Dugan <dugan.pat@gmail.com>
2025-05-05 20:53:00 -0700

    Bunch of logic added

--- src/ui/navigation.tsx
+++ src/ui/navigation.tsx
@@ -1,37 +1,20 @@
+import { Tabs } from 'figma-kit';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-function Navigation() {
+function Navigation({ screen }) {
   return (
-    <nav className='grid grid-cols-3 gap-x-4 gap-y-2 justify-start content-center my-1 p-1 text-sm'>
-      <div>
-        <NavLink
-          to='/PluginScreen'
-          className={({ isActive, isPending }) =>
-            [
-              isPending ? 'text-teal-500' : '',
-              isActive ? 'text-slate-900' : 'text-slate-500',
-              'hover:underline hover:text-teal-500',
-            ].join(' ')
-          }
-        >
-          Home
-        </NavLink>
-      </div>
-      <div>
-        <NavLink
-          to='/SettingsScreen'
-          className={({ isActive, isPending }) =>
-            [
-              isPending ? 'text-teal-500' : '',
-              isActive ? 'text-slate-900' : 'text-slate-500',
-              'hover:underline hover:text-teal-500',
-            ].join(' ')
-          }
-        >
-          Settings
-        </NavLink>
-      </div>
+    <nav>
+      <Tabs.Root defaultValue={screen}>
+        <Tabs.List>
+          <Tabs.Trigger value='home'>
+            <NavLink to='/PluginScreen'>Home</NavLink>
+          </Tabs.Trigger>
+          <Tabs.Trigger value='settings'>
+            <NavLink to='/SettingsScreen'>Settings</NavLink>
+          </Tabs.Trigger>
+        </Tabs.List>
+      </Tabs.Root>
     </nav>
   );
 }

f241c63791733fae4500ccba2efa756de3d83eeb
Patrick Dugan <dugan.pat@gmail.com>
2025-05-05 20:53:00 -0700

    Bunch of logic added

--- src/ui/screens/settings.tsx
+++ src/ui/screens/settings.tsx
@@ -1,43 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AlertDialog, Text, Button, Input } from 'figma-kit';
 import Navigation from '../navigation';
-import { Title, Textarea, Button, Select } from 'react-figma-plugin-ds';
+
+const screen = 'settings';
+const keyLength = 108;
 
 function SettingsScreen({}) {
-  return (
-    <div className='px-4 py-1'>
-      <Navigation />
-      <h1 className='text-xl font-bold'>Page 2...</h1>
-      <Title size='large' weight='bold'>
-        My plugin
-      </Title>
+  const [key, setKey] = useState('');
+  const [keyIsValid, setKeyIsValid] = useState(false);
 
-      <Textarea
-        className='standard'
-        defaultValue=''
-        onChange={function _() {}}
-        placeholder='Placeholder text...'
-        rows={2}
-      />
-      <Select
-        className='standrdDropdown'
-        defaultValue='1'
-        onChange={function _() {}}
-        onExpand={function _() {}}
-        options={[
-          {
-            label: 'Item 1',
-            title: 'Item 1 description',
-            value: 1,
-          },
-          {
-            label: 'Item 2',
-            title: 'Item 2 description',
-            value: 2,
-          },
-        ]}
-        placeholder='Placeholder text...'
-      />
-      <Button>Start</Button>
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+
+    setKey(value);
+    setKeyIsValid(value.length === keyLength);
+  };
+
+  const renderNav = () => {
+    return <Navigation screen={screen} />;
+  };
+
+  const renderKey = () => {
+    return (
+      <div className='row-item'>
+        <Text className='heading'>Anthropic API key</Text>
+        <AlertDialog.Root>
+          <AlertDialog.Trigger>
+            <Button variant='secondary'>Add key</Button>
+          </AlertDialog.Trigger>
+          <AlertDialog.Overlay />
+          <AlertDialog.Content>
+            <AlertDialog.Title>Add key</AlertDialog.Title>
+            <AlertDialog.Description>Enter your Anthropic API key.</AlertDialog.Description>
+            <Input
+              name='anthropicKey'
+              placeholder='Ex. sk-ant-api03-nCjQtbsOljqx4VkPL'
+              value={key}
+              onChange={handleInputChange}
+              maxLength={keyLength}
+            />
+            <AlertDialog.Actions>
+              <AlertDialog.Cancel>
+                <Button>Cancel</Button>
+              </AlertDialog.Cancel>
+              <AlertDialog.Cancel>
+                <Button variant='primary' disabled={!keyIsValid}>
+                  Save
+                </Button>
+              </AlertDialog.Cancel>
+            </AlertDialog.Actions>
+          </AlertDialog.Content>
+        </AlertDialog.Root>
+      </div>
+    );
+  };
+
+  const renderBody = () => {
+    return <div className='body'>{renderKey()}</div>;
+  };
+
+  return (
+    <div>
+      {renderNav()}
+      {renderBody()}
     </div>
   );
 }

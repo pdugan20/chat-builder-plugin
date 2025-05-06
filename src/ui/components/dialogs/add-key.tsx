8bf78fad6358622b26e4298ca2623050445db502
Patrick Dugan <dugan.pat@gmail.com>
2025-05-06 10:24:31 -0700

    wip

---
+++ src/ui/components/dialogs/add-key.tsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import { AlertDialog, Button, Input, Link } from 'figma-kit';
+
+const keyLength = 108;
+
+function AddKeyDialog({ setKey, apiKey }): React.JSX.Element {
+  const [keyIsValid, setKeyIsValid] = useState(false);
+
+  interface HandleInputChangeProps {
+    updatedKey: string;
+  }
+
+  function handleInputChange({ updatedKey }: { updatedKey: HandleInputChangeProps['updatedKey'] }): void {
+    setKey(updatedKey);
+    setKeyIsValid(updatedKey.length === keyLength);
+  }
+
+  function renderTrigger(): React.JSX.Element {
+    return (
+      <AlertDialog.Trigger>
+        <Link href='#'>Add key</Link>
+      </AlertDialog.Trigger>
+    );
+  }
+
+  const handleCancelClick = () => {
+    console.log('cancel');
+  };
+
+  function renderBody(): React.JSX.Element {
+    return (
+      <div>
+        <AlertDialog.Title>Add key</AlertDialog.Title>
+        <AlertDialog.Description>Enter your Anthropic API key.</AlertDialog.Description>
+        <Input
+          name='anthropicKey'
+          placeholder='Ex. sk-ant-api03-nCjQtbsOljqx4VkPL'
+          value={apiKey}
+          onChange={(e) => handleInputChange({ updatedKey: e.target.value })}
+          maxLength={keyLength}
+        />
+      </div>
+    );
+  }
+
+  function renderActions(): React.JSX.Element {
+    return (
+      <AlertDialog.Actions>
+        <AlertDialog.Cancel>
+          <Button onClick={handleCancelClick}>Cancel</Button>
+        </AlertDialog.Cancel>
+        <AlertDialog.Action>
+          <Button variant='primary' disabled={!keyIsValid}>
+            Save
+          </Button>
+        </AlertDialog.Action>
+      </AlertDialog.Actions>
+    );
+  }
+
+  function renderContent(): React.JSX.Element {
+    return (
+      <AlertDialog.Content>
+        {renderBody()}
+        {renderActions()}
+      </AlertDialog.Content>
+    );
+  }
+
+  function renderOverlay(): React.JSX.Element {
+    return <AlertDialog.Overlay />;
+  }
+
+  return (
+    <AlertDialog.Root>
+      {renderTrigger()}
+      {renderOverlay()}
+      {renderContent()}
+    </AlertDialog.Root>
+  );
+}
+
+export default AddKeyDialog;

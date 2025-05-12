bfce515beefc4165d1cff740fba957a1a3a46ce2
Patrick Dugan <dugan.pat@gmail.com>
2025-05-12 15:00:52 -0700

    Lets lock this in now

---
+++ src/scripts/load-fonts.ts
@@ -0,0 +1,40 @@
+export default async function loadFonts() {
+  const requiredFonts = [
+    {
+      family: 'SF Pro Text',
+      style: 'Medium',
+    },
+    {
+      family: 'SF Pro Text',
+      style: 'Regular',
+    },
+  ];
+
+  let areRequiredFontsAvailable = false;
+
+  try {
+    const fonts = await figma.listAvailableFontsAsync();
+    areRequiredFontsAvailable = requiredFonts.every((requiredFont) =>
+      fonts.some((font) => font.fontName.family === requiredFont.family && font.fontName.style === requiredFont.style)
+    );
+
+    if (areRequiredFontsAvailable) {
+      await Promise.all(
+        requiredFonts.map(async (requiredFont) => {
+          const fontToLoad = fonts.find(
+            (font) => font.fontName.family === requiredFont.family && font.fontName.style === requiredFont.style
+          );
+          if (fontToLoad) {
+            await figma.loadFontAsync(fontToLoad.fontName);
+          }
+        })
+      );
+    }
+  } catch (err) {
+    //
+  }
+  figma.ui.postMessage({
+    type: 'LOAD_REQUIRED_FONTS',
+    hasFonts: areRequiredFontsAvailable,
+  });
+}

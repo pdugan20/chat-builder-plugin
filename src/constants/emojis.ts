5d9f998f673176647f1dcda00fa7b26f16842a04
Patrick Dugan <dugan.pat@gmail.com>
2025-05-12 23:17:34 -0700

    Pause there

---
+++ src/constants/emojis.ts
@@ -0,0 +1,20 @@
+const emojiStyle = {
+  color: {
+    heart: '246:145',
+    thumbsUp: '439:6215',
+    thumbsDown: '439:6373',
+    haha: '246:1079',
+    exclamation: '246:1077',
+    question: '439:6843',
+  },
+  transparentBlue: {
+    heart: '246:145',
+    thumbsUp: '439:7001',
+    thumbsDown: '246:1640',
+    haha: '246:1642',
+    exclamation: '439:7626',
+    question: '439:7784',
+  },
+};
+
+export default emojiStyle;

9f2008eb7adffdb33bcc456cea7de9050c8220c6
Patrick Dugan <dugan.pat@gmail.com>
2025-05-12 23:17:34 -0700

    Pause there

---
+++ src/constants/collections.ts
@@ -0,0 +1,11 @@
+export const colorCollection = {
+  key: '02d48d3f137ce91e1c40b559d1ca0bdeced04a30',
+  id: 'VariableCollectionId:02d48d3f137ce91e1c40b559d1ca0bdeced04a30/594:75',
+  libraryName: 'iMessage Chat Builder',
+  name: 'Color',
+};
+
+export const modeId = {
+  light: '76:0',
+  dark: '76:1',
+};

507fd78accb3b5f2180b476c9ceb7f1e48cf8d60
Patrick Dugan <dugan.pat@gmail.com>
2025-05-12 19:26:16 -0700

    OK

--- src/scripts/build-chat-ui.ts
+++ src/scripts/build-chat-ui.ts
@@ -6,9 +6,9 @@ interface BuildChatUserInterfaceProps {
   width?: number;
 }
 
-// 'First bubble#80:3': 'Testing',
-// 'Second bubble#80:4': 'Testing',
-// 'Third bubble#80:7': 'Testing',
+// 'First bubble#80:3'
+// 'Second bubble#80:4'
+// 'Third bubble#80:7'
 
 export default async function buildChatUserInterface({ data, width = 402 }: BuildChatUserInterfaceProps) {
   // eslint-disable-next-line no-console
@@ -16,7 +16,7 @@ export default async function buildChatUserInterface({ data, width = 402 }: Buil
 
   const senderSet = await figma.importComponentSetByKeyAsync(componentKey.senderBubble);
   const recipientSet = await figma.importComponentSetByKeyAsync(componentKey.recipientBubble);
-  console.log(senderSet.componentPropertyDefinitions);
+  // console.log(recipientSet.componentPropertyDefinitions);
 
   const frame = figma.createFrame();
   frame.layoutMode = 'VERTICAL';
@@ -25,31 +25,19 @@ export default async function buildChatUserInterface({ data, width = 402 }: Buil
   // const recipientBubbleSet = await figma.importComponentSetByKeyAsync(componentKey.recipientBubble);
   // console.log(recipientBubbleSet);
 
-  chatData.forEach((item, index) => {
+  chatData.forEach((item) => {
     const { role, message, messagesInGroup } = item;
 
     if (role === 'sender') {
       const senderInstance = senderSet.defaultVariant.createInstance();
 
-      const properties: Record<string, string> = {
-        Bubbles: messagesInGroup === 3 ? '3' : messagesInGroup === 2 ? '2' : '1',
+      senderInstance.setProperties({
+        Bubbles: '1',
         Style: 'iOS',
         'Has reaction': 'No',
         'Has mustache text': 'No',
         'First bubble#80:3': message,
-      };
-
-      // If messagesInGroup is 2 or 3, set the 'Second bubble' property
-      if (messagesInGroup >= 2 && chatData[index + 1]) {
-        properties['Second bubble#80:4'] = chatData[index + 1].message;
-      }
-
-      // If messagesInGroup is 3, set the 'Third bubble' property
-      if (messagesInGroup === 3 && chatData[index + 2]) {
-        properties['Third bubble#80:7'] = chatData[index + 2].message;
-      }
-
-      senderInstance.setProperties(properties);
+      });
 
       senderInstance.resize(width, senderInstance.height);
       frame.appendChild(senderInstance);
@@ -58,35 +46,23 @@ export default async function buildChatUserInterface({ data, width = 402 }: Buil
     if (role === 'recipient') {
       const recipientInstance = recipientSet.defaultVariant.createInstance();
 
-      const properties: Record<string, string> = {
-        Bubbles: messagesInGroup === 3 ? '3' : messagesInGroup === 2 ? '2' : '1',
+      recipientInstance.setProperties({
+        Bubbles: '1',
         'Is group chat': 'No',
         'Has reaction': 'No',
         'Has mustache text': 'No',
         'First bubble#41:2': message,
-      };
+      });
 
-      // If messagesInGroup is 2 or 3, set the 'Second bubble' property
-      if (messagesInGroup >= 2 && chatData[index + 1]) {
-        properties['Second bubble#40:1'] = chatData[index + 1].message;
-      }
-
-      // If messagesInGroup is 3, set the 'Third bubble' property
-      if (messagesInGroup === 3 && chatData[index + 2]) {
-        properties['Third bubble#45:0'] = chatData[index + 2].message;
-      }
-
-      recipientInstance.setProperties(properties);
-
+      const currentTransform = recipientInstance.relativeTransform;
       recipientInstance.relativeTransform = [
-        [-1, 0, recipientInstance.relativeTransform[0][2]], // Flip horizontally
-        [0, 1, recipientInstance.relativeTransform[1][2]], // Keep vertical scale unchanged
+        [-1, 0, currentTransform[0][2]], // Flip on y-axis
+        [0, 1, currentTransform[1][2]], // Ignore x-axis
       ];
 
       recipientInstance.resize(width, recipientInstance.height);
       frame.appendChild(recipientInstance);
     }
-
-    console.log(role, message, messagesInGroup);
+    // console.log(role, message, messagesInGroup);
   });
 }

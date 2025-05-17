2f6136640509111f7c024a2b8f5a0eb71ae245ee
Patrick Dugan <dugan.pat@gmail.com>
2025-05-16 20:07:10 -0700

    KK

---
+++ src/ui/components/api-key-overlay.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Text, Link, AlertDialog } from 'figma-kit';
+import UpdateKeyDialog from './dialogs/update-key';
+import { useAnthropic } from '../context/anthropic';
+
+function ApiKeyOverlay(): React.JSX.Element {
+  const { anthropicKey } = useAnthropic();
+
+  return (
+    <div className='fixed inset-0 z-50 flex items-center justify-center bg-[var(--figma-color-bg)]'>
+      <div className='flex flex-col items-center px-4 text-center'>
+        <Text size='large' className='text-lg font-bold mb-1 text-[var(--figma-color-text)]'>
+          Get started
+        </Text>
+        <Text>In order to use this plugin, you must have an Anthropic account with an API key.</Text>
+        <Link href='https://docs.anthropic.com/en/api/overview' target='_blank' className='mb-4'>
+          Learn more
+        </Link>
+        <AlertDialog.Root>
+          <UpdateKeyDialog
+            dialogTitle='Add key'
+            anthropicKey={anthropicKey}
+            actionLabel='Add key'
+            buttonVariant='primary'
+          />
+        </AlertDialog.Root>
+      </div>
+    </div>
+  );
+}
+
+export default ApiKeyOverlay;

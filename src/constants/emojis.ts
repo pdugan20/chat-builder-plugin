54356faeea37690324fe019897c7b063f7a9b2df
Patrick Dugan <dugan.pat@gmail.com>
2025-05-23 12:08:53 -0700

    Remove hard-coded key/id from constants for components

--- src/constants/emojis.ts
+++ src/constants/emojis.ts
@@ -1,20 +1,6 @@
 const emojiKey = {
-  color: {
-    heart: { key: '813b6a40ee554c44300f9235ee882143606fd3d0' },
-    thumbsUp: { key: 'f017bd960d7d9852f3be68f6168cd8b0d53bf518' },
-    thumbsDown: { key: 'eaefc2142233c1dad46b4c1b5d83f88bbb2595f7' },
-    haha: { key: 'fe85bd457cb1305893435956defa90495fea0e59' },
-    exclamation: { key: 'bf58bc1d6cc28366525db057a0a28f575b679a46' },
-    question: { key: '38696a779e561aac647f19d5f0b173a97c3260c6' },
-  },
-  transparentBlue: {
-    heart: { key: '813b6a40ee554c44300f9235ee882143606fd3d0' },
-    thumbsUp: { key: '277d9f92849b5f3613a2b1da7339d6a333cdec17' },
-    thumbsDown: { key: '925ab413026302982376a2fd029b36b3bc41ba46' },
-    haha: { key: '301319bf7c8bc6287f71226bd6262ba77a567240' },
-    exclamation: { key: '90c2fa68ad5683ef474aa913d385d4bb842a064a' },
-    question: { key: 'b1019ffdc9de4be59bb05494e74fc36bb970b5a9' },
-  },
+  color: {},
+  transparentBlue: {},
 };
 
 export default emojiKey;
